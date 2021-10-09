188fbb3430bd6b9842e1709dd3b3eb5b8027509d
Meshiest <meek.mesh@gmail.com>
2021-10-08 23:44:28 -0500

    feat(web-ui): updated chat font

--- src/webserver/frontend/widgets/Chat.vue
+++ src/webserver/frontend/widgets/Chat.vue
@@ -12,6 +12,7 @@
   .messages {
     flex: 1;
     position: relative;
+    font-family: 'Roboto';
 
     .messages-child {
       @include column;
@@ -24,7 +25,6 @@
     width: calc(100% - 24px - 20px);
   }
 }
-
 </style>
 
 <template>
@@ -33,17 +33,23 @@
       <div class="messages-child">
         <div v-for="log in chats" :key="log._id" class="log-entry">
           <div v-if="log.action === 'msg'" class="chat-message">
-            {{log.user.web ? '[' : ''}}<span class="user" :style="{color: '#'+log.user.color}"
-            >{{log.user.name}}</span>{{log.user.web ? ']' : ''}}: <span v-html="xss(log.message)" v-linkified />
+            {{ log.user.web ? '[' : ''
+            }}<span class="user" :style="{ color: '#' + log.user.color }">{{
+              log.user.name
+            }}</span
+            >{{ log.user.web ? ']' : '' }}:
+            <span v-html="xss(log.message)" v-linkified />
           </div>
           <div v-if="log.action === 'leave'" class="join-message">
-            <span class="user">{{log.user.name}}</span> left the game.
+            <span class="user">{{ log.user.name }}</span> left the game.
           </div>
           <div v-if="log.action === 'join'" class="join-message">
-            <span class="user">{{log.user.name}}</span> joined the game{{log.user.isFirst ? ' for the first time' : ''}}.
+            <span class="user">{{ log.user.name }}</span> joined the game{{
+              log.user.isFirst ? ' for the first time' : ''
+            }}.
           </div>
           <div v-if="log.action === 'server'" class="server-message">
-            {{log.message}}
+            {{ log.message }}
           </div>
         </div>
       </div>
@@ -59,10 +65,8 @@
   </div>
 </template>
 <script>
-
 import Vue from 'vue';
 import SendIcon from 'vue-tabler-icons/icons/SendIcon';
-
 
 export default Vue.component('br-chat-widget', {
   components: { SendIcon },
@@ -71,8 +75,7 @@ export default Vue.component('br-chat-widget', {
       this.chats.push(log);
 
       // if the log is too long - remove the excess messages (you should probably be in the history view)
-      if (this.chats.length > 50)
-        this.chats.splice(0, 50 - this.chats.length);
+      if (this.chats.length > 50) this.chats.splice(0, 50 - this.chats.length);
 
       this.scroll();
     },
@@ -105,8 +108,7 @@ export default Vue.component('br-chat-widget', {
     sendMessage(event) {
       event.preventDefault();
 
-      if (this.message.length > 140 || this.message.length === 0)
-        return;
+      if (this.message.length > 140 || this.message.length === 0) return;
 
       this.$$notify('chat', this.message);
       this.message = '';
@@ -116,8 +118,7 @@ export default Vue.component('br-chat-widget', {
     return {
       chats: [],
       message: '',
-    }
+    };
   },
 });
-
 </script>
