6782525238fd3415bd1765b365967349aa9819aa
Zander <zander@zanderf.net>
2022-04-28 09:32:59 -0500

    fix: rust template scripts

--- templates/rust/setup.sh
+++ templates/rust/setup.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
 # This script will be run when the plugin is installed...
+if command -v cargo; then
+  cargo build --release
+else
+  echo "WARNING: Rust is not installed. This plugin could not be built. It will not work unless it has a bundled binary."
+fi
